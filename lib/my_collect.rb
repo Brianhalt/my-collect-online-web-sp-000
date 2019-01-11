0bef2fafc98293d2719204da8900d1bb65aa76cf
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-11 14:32:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,10 @@
 
 def my_collect(collection)
   i = 0
-  collection = []
-  while i < coillectoin.length
-    collection << yield(collection[i])
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
     i += 1
   end
-  collection
-  end
+  new_collection
 end
